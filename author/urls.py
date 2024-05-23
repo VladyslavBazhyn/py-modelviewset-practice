83dbc95d0c9c9c705d6d1a615e2713fe33a1ab3c
Vlad_work <mr.darmstadtium@gmail.com>
2024-05-23 11:41:37 +0200

    pass all tests

--- author/urls.py
+++ author/urls.py
@@ -1,3 +1,21 @@
-# Create your urls here
+from django.urls import path, include
+from author.views import AuthorViewSet
+# AuthorList, AuthorDetail
+app_name = "author"
 
-urlpatterns = []
+urlpatterns = [
+    path("authors/", AuthorViewSet.as_view(
+        {
+            "get": "list",
+            "post": "create"
+        }
+    ), name="manage-list"),
+    path("authors/<int:pk>/", AuthorViewSet.as_view(
+        {
+            "get": "retrieve",
+            "put": "update",
+            "patch": "partial_update",
+            "delete": "destroy"
+        }
+    ), name="manage-detail")
+]
